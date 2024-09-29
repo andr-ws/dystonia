eebe3420abc285ee68d30d912f7399a16b973767
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-09-29 08:39:58 +0100

    Update flip_rh_vta.m

--- flip_rh_vta.m
+++ flip_rh_vta.m
@@ -9,7 +9,7 @@ for i = 1:length(ids)
     id = ids(i);
     
     % Define input and output file paths
-    input_file = sprintf('/Users/neuro-239/imaging/datasets/cd/derivatives/sub-%d/stimulations/MNI152NLin2009bAsym/gs_direc/sub-%d_sim-binary_model-simbio_hemi-R.nii', id, id);
+    input_file = sprintf('/Users/neuro-239/imaging/datasets/cd/derivatives/leaddbs/sub-%d/stimulations/MNI152NLin2009bAsym/gs_direc/sub-%d_sim-binary_model-simbio_hemi-R.nii', id, id);
     output_file = sprintf('/Users/neuro-239/imaging/datasets/cd/derivatives/leadgroup/direc/vtas/sub-%d_sim-binary_model-simbio_hemi-R-to-L.nii', id);
     
     % Call the function ea_flip_lr_nonlinear
