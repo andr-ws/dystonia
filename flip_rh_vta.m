78d9ec26facc9f0fd083a5367d69f9cfcab13ea6
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-09-29 08:37:44 +0100

    Create flip_rh_vta.m

---
+++ flip_rh_vta.m
@@ -0,0 +1,17 @@
+% Create the directory
+mkdir('./imaging/datasets/cd/derivatives/leadgroup/direc/vtas');
+
+% List of IDs
+ids = [10, 14, 15, 17, 24, 30, 31, 34, 35, 37, 38, 40, 48, 51, 52, 53, 54, 55, 56, 59, 60, 61];
+
+% Loop over each ID
+for i = 1:length(ids)
+    id = ids(i);
+    
+    % Define input and output file paths
+    input_file = sprintf('./imaging/datasets/cd/derivatives/sub-%d/stimulations/MNI152NLin2009bAsym/gs_direc/sub-%d_sim-binary_model-simbio_hemi-R.nii', id, id);
+    output_file = sprintf('./imaging/datasets/cd/derivatives/leadgroup/direc/vtas/sub-%d_sim-binary_model-simbio_hemi-R-to-L.nii', id);
+    
+    % Call the function ea_flip_lr_nonlinear
+    ea_flip_lr_nonlinear(input_file, output_file, 1);
+end
